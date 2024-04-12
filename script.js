01740f2aca35b624bf5bdf7734b2f7bda320bf2e
Mikel Aramendia <mikel.aramendia.lacalle@gmail.com>
2024-04-12 07:14:46 +0200

    Restored hero and added nav

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var hero = document.querySelector('.hero');
     hero.classList.add('fade-in');
 });
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  
