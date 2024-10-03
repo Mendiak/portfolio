6f5b012aff14c7b31b37d22a9519da65bb0bfbe8
Mikel Aramendia <mikel.aramendia.lacalle@gmail.com>
2024-10-03 06:40:19 +0200

    Hamburguer menu repaired

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ document.addEventListener('DOMContentLoaded', function() {
 function scrollToSection(sectionId) {
   let section = document.getElementById(sectionId);
   if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
+    section.scrollIntoView({ behavior: 'smooth' });
+    // Cerrar el menú móvil
+    let menu = document.querySelector('.mobile-menu');
+    menu.classList.remove('active');
+    // También resetear el botón de hamburguesa si es necesario
+    let hamburger = document.getElementById("hamburger-btn");
+    hamburger.classList.remove('active');
   }
 }
 
+
 function toggleMobileMenu() {
-  let menu = document.getElementById("mobile-menu");
-  menu.classList.toggle("active");
+  let menu = document.querySelector('.mobile-menu');
+  menu.classList.toggle('active');
+ 
 }
+
